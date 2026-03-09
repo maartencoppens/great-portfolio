3cf15890a346758e822270254e3d6342530b955a
Maarten Coppens <maarcopp@student.arteveldehs.be>
2026-03-09 11:58:16 +0100

    Add layout components and update global styles; remove unused assets

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full flex flex-row justify-between items-center py-l px-xl bg-bg-secondary text-text-tertiary">
+      <p>© 2026 · Designed & Developed with care</p>
+      <p>Available for freelance opportunities</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full flex flex-row justify-between items-center p-4">
+      <p>© 2026 · Designed & Developed with care</p>
+      <p>Available for freelance opportunities</p>
+    </div>
+  );
+};
+
+export default Navbar;
