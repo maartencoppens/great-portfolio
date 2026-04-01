c41c0f6babe96c5f547fec6222b409981b7a4f80
Maarten Coppens <maarcopp@student.arteveldehs.be>
2026-04-01 15:04:40 +0200

    Remove Loader component from the main page

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,6 @@ export default function Home(): JSX.Element {
                   <Model />
                 </Suspense>
               </Canvas>
-              <Loader />
             </div>
           </div>
         </section>
